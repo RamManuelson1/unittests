997b4cb24996c2642c6fbf8ecfb1f5510ecdc51d
Roman Sharoyan <sharoyan24@yandex.ru>
2023-07-26 19:52:48 +0300

    additions to tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,5 +12,24 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_slice_with_negative_start_index(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -3, 4), [3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -2), [4, 5])
+
+    def test_slice_with_negative_end_index(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], 1, -1), [2, 3, 4])
+
+    def test_slice_with_negative_indices_out_of_range(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -7, -6), [])
+
+    def test_slice_with_start_index_greater_than_end_index(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], 3, 1), [])
+
+    def test_slice_with_end_index_equal_to_start_index(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], 2, 2), [])
+
+    def test_slice_with_end_index_greater_than_length(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], 2, 10), [3, 4, 5])
+    def test_slice_with_default_arguments(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+        self.assertEqual(arrs.my_slice([]), [])
